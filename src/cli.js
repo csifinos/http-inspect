36c02efb14f7aa10d9351ec882087addb8742ffd
Christos Sifinos <cn.sifinos@gmail.com>
2024-11-30 00:45:02 +0100

    Add version

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,28 @@
 const minimist = require('minimist');
+const { version } = require('./version');
+
+const VERSION = version;
 
 function parseArguments(argv) {
   const args = minimist(argv, {
     string: ['port', 'target'],
-    alias: { p: 'port', t: 'target', h: 'help' },
+    boolean: ['help', 'version'],
+    alias: { p: 'port', t: 'target', h: 'help', v: 'version' },
   });
 
-  const shouldPrintHelp = args.help || (!args.target || !args.port);
-
-  if (shouldPrintHelp) {
+  if (args.help) {
     printHelp();
     process.exit(0);
+  }
+
+  if (args.version) {
+    printVersion();
+    process.exit(0);
+  }
+
+  if (!args.target || !args.port) {
+    printHelp();
+    process.exit();
   }
 
   return args;
@@ -18,18 +30,23 @@ function parseArguments(argv) {
 
 function printHelp() {
   console.log(`
-  Usage: http-inspect [options]
+  Usage: http-inspect [options] v${VERSION}
 
   A lightweight CLI tool for logging and inspecting HTTP requests and responses for development purposes.
 
   Options:
     -p, --port <number>     Port to run the proxy server on
     -t, --target <url>      Target URL to forward requests to
+    -v, --version           Show the version
     -h, --help              Display this help message
 
   Example:
     http-inspect --port 3000 --target http://example.com
   `);
+}
+
+function printVersion() {
+  console.log(`${VERSION}`);
 }
 
 module.exports = { parseArguments };
